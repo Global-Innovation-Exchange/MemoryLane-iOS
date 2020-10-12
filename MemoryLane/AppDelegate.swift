ba810d4e059192a0ad7eeaf0560fd16437a0cc7a
qpskcn1 <yizheng0831@gmail.com>
2020-10-12 03:09:09 -0700

    Remove launch page delay, it doesn't work any more with Swift 5

--- MemoryLane/AppDelegate.swift
+++ MemoryLane/AppDelegate.swift
@@ -13,7 +13,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        RunLoop.current.run(until: NSDate(timeIntervalSinceNow:2) as Date)
         return true
     }
 
