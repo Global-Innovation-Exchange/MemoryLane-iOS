4341bbe97e8b8779c4c54c612736cb1719dc96df
qpskcn1 <yizheng0831@gmail.com>
2020-10-11 23:12:37 -0700

    Add possible rectangle detection solution for button press.

--- MemoryLane/ViewController.swift
+++ MemoryLane/ViewController.swift
@@ -7,50 +7,98 @@
 
 import UIKit
 import AVFoundation
+import Vision
 
 let WIDTH = UIScreen.main.bounds.width
 let HEIGHT = UIScreen.main.bounds.height
+let BRIGHTNESS_THRESHOLD: CGFloat = 0.5
 
 class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
     
-    let captureSession = AVCaptureSession()
-    var frontCamera: AVCaptureDevice?
-    let previewLayer = CALayer()
-    let textlayer = CATextLayer()
+    private let captureSession = AVCaptureSession()
+    private lazy var previewLayer = AVCaptureVideoPreviewLayer(session: self.captureSession)
+    private let videoDataOutput = AVCaptureVideoDataOutput()
+    var totalFrameCount = 0
     var shapeLayers = [CAShapeLayer]()
-    var transform = CGAffineTransform.identity
-    // boolean for button press
-    static var buttonPressed: Bool = false
-    
-    // camera data frame data queue
-    let queue = DispatchQueue(label: "com.camera.video.queue")
-    
+    var player: AVAudioPlayer?
+    var buttonPressed = [Bool](repeating: false, count: 4)
     // color picker point
     var pickers = [
-        CGPoint(x: WIDTH/5, y: HEIGHT/10),
-        CGPoint(x: WIDTH/5*2, y: HEIGHT/10),
-        CGPoint(x: WIDTH/5*3, y: HEIGHT/10),
-        CGPoint(x: WIDTH/5*4, y: HEIGHT/10),
+        CGPoint(x: WIDTH/3*0+5, y: 60),
+        CGPoint(x: WIDTH/3*1, y: 60),
+        CGPoint(x: WIDTH/3*2, y: 60),
+        CGPoint(x: WIDTH-5, y: 60),
     ]
     
-    let buttonNames = ["Like", "Repeat", "Next", "Turn Off"]
-        
+    let buttonColors = [UIColor.green, UIColor.red, UIColor.yellow, UIColor.blue]
+    let buttonNames = ["Like", "Repeat", "Next", "Play/Pause"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.setupUI()
-        self.createUI()
+        // connect Main Storyboard and create the button rect from scanAreaImage view
+//        let buttonRect: CGRect = scanAreaImage.frame
+//        regionOfInterest = buttonRect
+        // set up camera, camera feed, camera output
+        self.setCameraInput()
+        self.showCameraFeed()
+        self.drawButtons(n: 4)
+        self.setCameraOutput()
     }
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-    if let touch = touches.first {
-        let location = touch.location(in: self.view)
-        print(location.x, location.y)
-        print(self.pickers)
-      }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+//        self.previewLayer.frame = self.view.bounds
     }
-
-    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+    
+    override func viewDidAppear(_ animated: Bool) {
+        //session Start
+        self.videoDataOutput.setSampleBufferDelegate(self,
+                                                     queue:DispatchQueue(label:"camera_frame_processing_queue"))
+        self.captureSession.startRunning()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        //session Stopped
+        self.videoDataOutput.setSampleBufferDelegate(nil, queue: nil)
+        self.captureSession.stopRunning()
+    }
+    
+    //Set the captureSession!
+    private func setCameraInput() {
+        guard let device = AVCaptureDevice.DiscoverySession(
+                deviceTypes: [.builtInWideAngleCamera,
+                              .builtInDualCamera,
+                              .builtInTrueDepthCamera],
+                mediaType: .video,
+                position: .front).devices.first else {
+            fatalError("No front camera device found.")
+        }
+        let cameraInput = try! AVCaptureDeviceInput(device: device)
+        self.captureSession.addInput(cameraInput)
+    }
+    
+    private func showCameraFeed() {
+        self.previewLayer.videoGravity = .resizeAspect
+//        self.view.layer.addSublayer(self.previewLayer)
+//        self.previewLayer.frame = self.view.frame
+        self.previewLayer.frame = CGRect(x: 0, y: 0, width: 810, height: 150)
+    }
+    
+    private func setCameraOutput() {
+        self.videoDataOutput.videoSettings = [(kCVPixelBufferPixelFormatTypeKey as NSString) : NSNumber(value: kCVPixelFormatType_32BGRA)] as [String : Any]
+        self.videoDataOutput.alwaysDiscardsLateVideoFrames = true
+        self.videoDataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "camera_frame_processing_queue"))
+        self.captureSession.addOutput(self.videoDataOutput)
+        guard let connection = self.videoDataOutput.connection(with: AVMediaType.video),
+              connection.isVideoOrientationSupported else { return }
+        connection.videoOrientation = .portrait
+    }
+    
+    func captureOutput(_ output: AVCaptureOutput,didOutput sampleBuffer: CMSampleBuffer,from connection: AVCaptureConnection) {
+        totalFrameCount += 1
+//        if totalFrameCount % 3 != 0{ return }
         guard let imageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {
+            debugPrint("unable to get image from sample buffer")
             return
         }
         CVPixelBufferLockBaseAddress(imageBuffer, CVPixelBufferLockFlags(rawValue: CVOptionFlags(0)))
@@ -58,118 +106,141 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
             return
         }
         let width = CVPixelBufferGetWidthOfPlane(imageBuffer, 0)
-        let height = CVPixelBufferGetHeightOfPlane(imageBuffer, 0)
+//        let height = CVPixelBufferGetHeightOfPlane(imageBuffer, 0)
         let bytesPerRow = CVPixelBufferGetBytesPerRowOfPlane(imageBuffer, 0)
         let colorSpace = CGColorSpaceCreateDeviceRGB()
         let bimapInfo: CGBitmapInfo = [
             .byteOrder32Little,
             CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedFirst.rawValue)]
-        
-        guard let content = CGContext(data: baseAddr, width: width, height: height, bitsPerComponent: 8, bytesPerRow: bytesPerRow, space: colorSpace, bitmapInfo: bimapInfo.rawValue) else {
+        let startPos = 130 * bytesPerRow
+        guard let buttonAreaContext = CGContext(data: baseAddr + startPos, width: width, height: 80, bitsPerComponent: 8, bytesPerRow: bytesPerRow, space: colorSpace, bitmapInfo: bimapInfo.rawValue) else {
             return
         }
         
-        guard let cgImage = content.makeImage() else {
+        guard let buttonImage = buttonAreaContext.makeImage() else {
             return
         }
+        self.detectRectangleByBrightness(in: buttonImage)
+//        self.detectRectangle(in: buttonImage)
+    }
+    
+    private func detectRectangle(in image: CGImage) {
+        let request = VNDetectRectanglesRequest(completionHandler: { (request: VNRequest, error: Error?) in
+            DispatchQueue.main.async {
+                self.previewLayer.contents = image
+                guard let results = request.results as? [VNRectangleObservation] else { return }
+                self.removeBoundingBoxLayer()
+                //retrieve the first observed rectangle
+                guard let rect = results.first else{return}
+                //function used to draw the bounding box of the detected rectangle
+                self.drawBoundingBox(rect: rect)
+            }
+        })
+        //Set the value for the detected rectangle
+//        request.minimumAspectRatio = VNAspectRatio(0.2)
+        request.maximumAspectRatio = VNAspectRatio(0.9)
+        request.quadratureTolerance = 20
+        request.minimumSize = Float(0.2)
+        request.maximumObservations = 1
+        request.minimumConfidence = 0.7
+        let imageRequestHandler = VNImageRequestHandler(cgImage: image, options: [:])
+        try? imageRequestHandler.perform([request])
+    }
+    
+    func drawBoundingBox(rect : VNRectangleObservation) {
+        // mirror layer
+        let transform = CGAffineTransform(scaleX: -1, y: -1)
+            .translatedBy(x: -self.previewLayer.bounds.width,
+                          y: -220)
+        let scale = CGAffineTransform.identity
+            .scaledBy(x: self.previewLayer.bounds.width,
+                      y: 220)
+        let bounds = rect.boundingBox
+            .applying(scale).applying(transform)
+        createLayer(in: bounds)
+        print("draw rect", rect.boundingBox.maxX)
+    }
+    
+    private var bBoxLayer = CAShapeLayer()
+    
+    private func createLayer(in rect: CGRect) {
+        bBoxLayer = CAShapeLayer()
+        bBoxLayer.frame = rect
+        bBoxLayer.cornerRadius = 10
+        bBoxLayer.opacity = 1
+        bBoxLayer.borderColor = UIColor.systemGreen.cgColor
+        bBoxLayer.borderWidth = 6.0
+        previewLayer.insertSublayer(bBoxLayer, at: 1)
+    }
+    
+    func removeBoundingBoxLayer() {
+        bBoxLayer.removeFromSuperlayer()
+    }
+    
+    private func detectRectangleByBrightness(in image: CGImage) {
         DispatchQueue.main.async {
-            self.previewLayer.contents = cgImage
+            self.previewLayer.contents = image
             for (n, picker) in self.pickers.enumerated() {
                 // Update the point after transformation
-                let position = picker.applying(self.transform)
-                if self.previewLayer.isLight(at: position) {
-                    if !ViewController.buttonPressed {
-                        self.shapeLayers[n].strokeColor = UIColor.green.cgColor
-                        self.showToast(self.buttonNames[n] + " Button Pressed")
-                        print("Button Pressed")
-                        print(ViewController.buttonPressed)
-                        ViewController.buttonPressed = true
-                    }
+//                let position = picker.applying(self.transform)
+                if self.previewLayer.isLight(at: picker) {
+//                    if !ViewController.buttonPressed {
+                    self.shapeLayers[3-n].fillColor = self.buttonColors[3-n].cgColor
+                    self.buttonPressed[3-n] = true
+//                        self.showToast(self.buttonNames[3-n] + " Button Pressed")
+//                        print("Button Pressed")
+//                        print(3-n, picker)
+//                        print(ViewController.buttonPressed)
+//                        ViewController.buttonPressed = true
+//                    }
                 } else {
-                    self.shapeLayers[n].strokeColor = UIColor.red.cgColor
-                    ViewController.buttonPressed = false
+                    self.shapeLayers[3-n].fillColor = UIColor.clear.cgColor
+                    self.buttonPressed[3-n] = false
+//                    ViewController.buttonPressed = false
                 }
             }
-        }
-        
-    }
-
-    func getFrontCamera() -> AVCaptureDevice? {
-        if let device = AVCaptureDevice.default(.builtInDualCamera,
-                                                for: AVMediaType.video,
-                                                position: .front) {
-            return device
-        } else if let device = AVCaptureDevice.default(.builtInWideAngleCamera,
-                            for: AVMediaType.video,
-                            position: .front) {
-            return device
-        } else {
-            return nil
-        }
-    }
-    
-    func setupUI() {
-        previewLayer.bounds = CGRect(x: 0, y: 0, width: WIDTH, height: WIDTH)
-        previewLayer.position = view.center
-        previewLayer.contentsGravity = CALayerContentsGravity.resizeAspectFill
-//        previewLayer.masksToBounds = true
-        // rotate by 90 degree
-        self.transform = self.transform.rotated(by: CGFloat(.pi / 2.0))
-        // mirror view
-        self.transform = self.transform.scaledBy(x: 1, y: -1)
-        previewLayer.setAffineTransform(self.transform)
-        view.layer.insertSublayer(previewLayer, at: 0)
-        for point in self.pickers {
-            let result = self.getIndicators(point: point)
-            let circle = result.circle
-            let dot = result.dot
-            view.layer.addSublayer(circle)
-            self.shapeLayers.append(circle)
-            view.layer.addSublayer(dot)
-        }
-    }
-    
-    func getIndicators(point: CGPoint) -> (circle: CAShapeLayer, dot: CAShapeLayer) {
-        // circle
-        let circlePath = UIBezierPath.init(ovalIn: CGRect.init(x: 0, y: 0, width: 40, height: 40))
-        let lineShape = CAShapeLayer()
-        lineShape.frame = CGRect.init(x: point.x-20, y:HEIGHT/5*4, width: 40, height: 40)
-        lineShape.lineWidth = 5
-        lineShape.strokeColor = UIColor.red.cgColor
-        lineShape.path = circlePath.cgPath
-        lineShape.fillColor = UIColor.clear.cgColor
-
-        // dot
-        let linePath1 = UIBezierPath.init(ovalIn: CGRect.init(x: 0, y: 0, width: 8, height: 8))
-        let lineShape1 = CAShapeLayer()
-        lineShape1.frame = CGRect.init(x: point.x-4, y:point.y+130, width: 8, height: 8)
-        lineShape1.path = linePath1.cgPath
-        lineShape1.fillColor = UIColor.init(white: 0.7, alpha: 0.8).cgColor
-        return (lineShape, lineShape1)
-    }
-    
-    func createUI() {
-        do {
-            self.frontCamera = self.getFrontCamera()
-            let captureDeviceInput = try AVCaptureDeviceInput(device: self.frontCamera!)
-            let videoOutput = AVCaptureVideoDataOutput()
-            videoOutput.videoSettings = [kCVPixelBufferPixelFormatTypeKey as AnyHashable: NSNumber(value: kCMPixelFormat_32BGRA)] as? [String : Any]
-            videoOutput.alwaysDiscardsLateVideoFrames = true
-            videoOutput.setSampleBufferDelegate(self, queue: queue)
-            
-            if self.captureSession.canAddOutput(videoOutput) {
-                self.captureSession.addOutput(videoOutput)
+            if self.buttonPressed.allSatisfy({$0}) {
+                print("all true")
             }
-            self.captureSession.addInput(captureDeviceInput)
-        } catch {
-            print(error)
-            return
         }
-        
-        self.captureSession.startRunning()
+    }
+    
+    private func drawButtons(n: Int) {
+        for i in 0...n-1 {
+            let circlePath = UIBezierPath.init(ovalIn: CGRect.init(x: 0, y: 0, width: 100, height: 100))
+            let lineShape = CAShapeLayer()
+            lineShape.frame = CGRect.init(x: 140*(i+1), y:Int(HEIGHT)/5*4, width: 100, height: 100)
+            lineShape.lineWidth = 5
+            lineShape.strokeColor = self.buttonColors[i].cgColor
+            lineShape.path = circlePath.cgPath
+            lineShape.fillColor = UIColor.clear.cgColor
+            view.layer.addSublayer(lineShape)
+            self.shapeLayers.append(lineShape)
+        }
+    }
+    
+    func playSound() {
+        guard let url = Bundle.main.url(forResource: "soundName", withExtension: "mp3") else { return }
+
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+            /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+
+            /* iOS 10 and earlier require the following line:
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
+
+            guard let player = player else { return }
+
+            player.play()
+
+        } catch let error {
+            print(error.localizedDescription)
+        }
     }
 }
-
 
 public extension CALayer {
     
@@ -179,7 +250,6 @@ public extension CALayer {
     ///
     /// - returns: Color
     func isLight(at position: CGPoint) -> Bool {
-        
         // pixel of the given point
         var pixel = [UInt8](repeatElement(0, count: 4))
         // set the colorspace to RGB
@@ -206,10 +276,12 @@ public extension UIColor {
         guard let components = cgColor.components, components.count > 2 else {return false}
         // w3.org/TR/AERT/#color-contrast
         let brightness = ((components[0] * 299) + (components[1] * 587) + (components[2] * 114)) / 1000
-        return (brightness > 0.85)
+//        if brightness > 0.6 {
+//            print(brightness)
+//        }
+        return (brightness > BRIGHTNESS_THRESHOLD)
     }
 }
-
 
 class ToastLabel: UILabel {
     var textInsets = UIEdgeInsets.zero {
@@ -230,15 +302,15 @@ class ToastLabel: UILabel {
 }
 
 extension UIViewController {
-    static let DELAY_SHORT = 0.1
-    static let DELAY_LONG = 0.1
+    static let DELAY_SHORT = 0.05
+    static let DELAY_LONG = 0.05
 
     func showToast(_ text: String, delay: TimeInterval = DELAY_LONG) {
         let label = ToastLabel()
         label.backgroundColor = UIColor(white: 0, alpha: 0.5)
         label.textColor = .white
         label.textAlignment = .center
-        label.font = UIFont.systemFont(ofSize: 35)
+        label.font = UIFont.systemFont(ofSize: 30)
         label.alpha = 0
         label.text = text
         label.clipsToBounds = true
